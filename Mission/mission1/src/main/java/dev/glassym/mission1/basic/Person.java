97ef0363f0cd9eb054312e4262cd3658287545d6
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-02-21 14:48:16 +0900

    Delete folder

--- Mission/mission1/src/main/java/dev/glassym/mission1/basic/Person.java
+++
@@ -1,5 +0,0 @@
-package dev.glassym.mission1.basic;
-
-public interface Person {
-    void speak();
-}
--- Mission/mission1/src/main/java/dev/glassym/mission1/challenge/ChallengeMain.java
+++
@@ -1,7 +0,0 @@
-package dev.glassym.mission1.challenge;
-
-public class ChallengeMain {
-    public static void main(String[] args) {
-
-    }
-}
