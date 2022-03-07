e1a97cf28aa0c06837b3cea66b1f1226d60958b3
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-03-07 15:54:43 +0900

    practice AOP

---
+++ week6/aop/src/main/java/dev/glassym/aop/PostController.java
@@ -0,0 +1,75 @@
+package dev.glassym.aop;
+
+import dev.glassym.aop.aspect.LogExecutionTime;
+import dev.glassym.aop.aspect.LogArguments;
+import dev.glassym.aop.aspect.LogReturn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("post")
+public class PostController {
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+    private final PostService postService;
+
+    public PostController(
+            @Autowired PostService postService
+    ){
+        this.postService = postService;
+    }
+
+    @LogArguments
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPost(
+            @RequestBody PostDto dto
+    ){
+        this.postService.createPost(dto);
+    }
+
+    @LogReturn
+    @GetMapping("{id}")
+    public PostDto readPost(
+            @PathVariable("id") int id
+    ){
+        return this.postService.readPost(id);
+    }
+
+    @LogExecutionTime
+    @GetMapping
+    public List<PostDto> readPostAll(){
+        return this.postService.readPostAll();
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updatePost(
+            @PathVariable("id") int id,
+            @RequestBody PostDto dto
+    ){
+        this.postService.updatePost(id, dto);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deletePost(
+            @PathVariable("id") int id
+    ){
+        logger.info("deletePost, id: {}", id);
+        this.postService.deletePost(id);
+    }
+
+    @GetMapping("test-log")
+    public void testLog(){
+        logger.trace("TRACE Log Message");
+        logger.debug("DEBUG Log Message");
+        logger.info("INFO Log Message");
+        logger.warn("WARN Log Message");
+        logger.error("ERROR Log Message");
+    }
+}
---
+++ week6/aop/src/main/java/dev/glassym/aop/aspect/LoggingAspect.java
@@ -0,0 +1,54 @@
+package dev.glassym.aop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // LogExecutionTime 어노테이션 주변에서 작동하는 Advice
+    @Around(value = "@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable{
+        long startTime = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long execTime = System.currentTimeMillis() - startTime;
+        logger.trace("method executed in {}", execTime);
+        return proceed;
+    }
+
+    @Before(value = "@annotation(dev.glassym.aop.aspect.LogArguments)")
+    public void logArguments(JoinPoint joinPoint){
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        logger.trace("method description : [{}]", methodSignature.getMethod());
+        logger.trace("method name: [{}]", methodSignature.getName());
+        logger.trace("declaring class: [{}]", methodSignature.getDeclaringType());;
+
+        Object[] arguments = joinPoint.getArgs();
+        if(arguments.length == 0){
+            logger.trace("no arguments");
+        }
+        for(Object argument: arguments){
+            logger.trace("argument: [{}]", argument);
+        }
+    }
+
+    @AfterReturning(value = "@annotation(LogReturn)", returning = "returnValue")
+    public void logResults(JoinPoint joinPoint, Object returnValue ){
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        logger.trace("method: [{}]", methodSignature.getName());
+        logger.trace("return type: [{}]", methodSignature.getReturnType());
+        logger.trace("return value: [{}]", returnValue);
+    }
+}
