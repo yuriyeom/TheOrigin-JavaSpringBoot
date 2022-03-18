5cff130f61e82b33c9602866fcf6fb73874a50bf
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-03-18 19:57:29 +0900

    practice login_logout

---
+++ week8/auth/src/main/java/dev/glassym/auth/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package dev.glassym.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 의존성 spring-security의 설정을 조작할 준비가 됐다.
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/home/**", "/user/**")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/home")
+                .permitAll()
+            .and()
+                .logout()
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/home")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .permitAll()
+        ;
+    }
+}
