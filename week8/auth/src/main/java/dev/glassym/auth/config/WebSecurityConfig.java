6bc22033a27d9fdc03ecf8ee51ba7fd8dd2c8c94
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-03-20 23:29:05 +0900

    practice login-custom

--- week8/auth/src/main/java/dev/glassym/auth/config/WebSecurityConfig.java
+++ week8/auth/src/main/java/dev/glassym/auth/config/WebSecurityConfig.java
@@ -1,13 +1,41 @@
 package dev.glassym.auth.config;
 
+import dev.glassym.auth.infra.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 의존성 spring-security의 설정을 조작할 준비가 됐다.
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private  final UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(
+            @Autowired CustomUserDetailsService customUserDetailsService
+            ) {
+        userDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication() // 메모리상에서 유저 검증을 하겠다.
+//                .withUser("user1") // 유저 추가
+//                .password(passwordEncoder().encode("user1pass"))
+//                .roles("USER")
+//                .and()
+//                .withUser("admin1")
+//                .password(passwordEncoder().encode("admin1pass"))
+//                .roles("ADMIN");
+        auth.userDetailsService(this.userDetailsService);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -29,5 +57,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .invalidateHttpSession(true)
                 .permitAll()
         ;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
